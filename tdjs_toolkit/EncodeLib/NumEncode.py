083f1f96b0304ea9ef4fab264902b81ffaacd68b
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2026-06-20 23:10:17 -0500

    some fixes. like making sure you only use Strings and making sure random numbers dont get fucked up by NumEncode

--- tdjs_toolkit/EncodeLib/NumEncode.py
+++ tdjs_toolkit/EncodeLib/NumEncode.py
@@ -1,8 +1,9 @@
 import json
+import struct
 
 
 class NumEncode:
-    def __init__(self, dictionary):
+    def __init__(self, dictionary: str):
 
         self.wordid = json.loads(dictionary)
 
@@ -11,20 +12,21 @@ class NumEncode:
         for k, v in self.wordid.items():
             self.idword[str(v)] = k
 
-    def encode(self, inpu):
+    def encode(self, inpu: str):
         out = ""
         for word in inpu.split():
             if word.lower() in self.wordid:
+                out += ".-"
                 out += f"{self.wordid[word.lower()]} "
             else:
                 out += f"{word.lower()} "
         return out
 
-    def decode(self, inpu):
+    def decode(self, inpu: str):
         outp = ""
         for pawn in inpu.split():
-            if pawn in self.idword:
-                outp += f"{self.idword[pawn]} "
+            if pawn.lstrip(".-") in self.idword:
+                outp += f"{self.idword[pawn.lstrip(".-")]} "
             else:
                 outp += f"{pawn} "
         return outp
